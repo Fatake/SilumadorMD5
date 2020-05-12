d5f0f72bf380c21e7cf071aeaa45d56ebeb8c656
Fatake Raicos <pace_lozano@hotmail.com>
2020-05-12 08:58:39 -0500

    Se agregaron comentarios a Servidor.java

--- Servidor.java
+++ Servidor.java
@@ -19,29 +19,43 @@ public class Servidor {
 	 * Main
 	 */
 	public static void main(String[] args) {
+		// Variables auxiliares
 		Servidor aux = new Servidor();
-		ServerSocket socketServer = null;
-		Socket socketDespachador = null;
 		LectorArchivo lec = new LectorArchivo();
+		// Socket Servidor
+		ServerSocket socketServer = null;
+		//Socket Cliente
+		Socket socketDespachador = null;
+
+		// Lee la base de datos de los usuarios
+		// Si no exsite, entonces sale
 		if ((aux.usuarios = lec.procesaArchivo("usuarios.dat"))== null) {
 			System.out.println("Error al leer usuarios.dat");
 			System.exit(-1);
 		}
+		
+		// imprime a los usuarios
 		aux.imprimeUsuarios();
+
+		// Intenta la coneccion con el socker servidor
 		try {
+			//                             Puerto
 			socketServer = new ServerSocket(9999);
 		} catch (IOException e) {
 			e.printStackTrace();
 			System.exit(-1);
 		}
 
+		// Inicia el servicio de escucha		
 		System.out.println("Escuchando: " + socketServer);
-        
+		
+		// Inicia el proceso de atender peticiones
 		while(true){
 			try {
+				// Se crea un socket despachador
 				socketDespachador = socketServer.accept();
 				System.out.println("Nueva conexion aceptada: " + socketDespachador);
-				//Escucha Peticiones
+				// Se crea un Hilo para esa peticion
 				new GestorPeticion(socketDespachador,aux.usuarios).start();
 				socketDespachador = null;
 			} 
@@ -52,6 +66,9 @@ public class Servidor {
 		}
 	}
 
+	/**
+	 * funcion que imprime usuarios de la Base de datos
+	 */
 	private void imprimeUsuarios(){
 		System.out.println("Usuarios en el Servidor\n<------------------------>");
 		for (Usuario usuario : usuarios) {
